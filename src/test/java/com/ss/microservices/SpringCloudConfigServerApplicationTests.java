6557b1e68127df9b0aa84599cf7cbc89980c4a64
Sadanala <ssadanala@deloitte.com>
2021-03-01 08:34:44 +0530

    config server changes

---
+++ src/test/java/com/ss/microservices/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
